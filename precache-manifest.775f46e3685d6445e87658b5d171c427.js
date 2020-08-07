82a899b206e7050acff03f558d4cd9746632ab6f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-07 02:48:41 +0000

    Deploy zhangwudi886/vantd-xdflib to github.com/zhangwudi886/vantd-xdflib.git:gh-pages

---
+++ precache-manifest.775f46e3685d6445e87658b5d171c427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1816dec36ed68bbcb8ac5cab06cf23",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.79107744523371ccbe49.bundle.js"
+  },
+  {
+    "url": "runtime~main.79107744523371ccbe49.bundle.js"
+  },
+  {
+    "revision": "bdea631666e9b2fdede5",
+    "url": "static/css/main.dfcc5bde.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.79107744523371ccbe49.bundle.js"
+  },
+  {
+    "url": "vendors~main.79107744523371ccbe49.bundle.js.LICENSE.txt"
+  }
+]);
