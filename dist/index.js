87f30ba837b9724fece7c9f9420f091a88e7994b
张世博 <zhangshibo@xdf.cn>
2020-08-07 10:22:32 +0800

    提交文件

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
 export { default as Button } from "./components/Button";
 export { default as Menu } from "./components/Menu";
+export { default as Upload } from "./components/Upload";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./styles/index.scss";
+// import App from "./App";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+// import * as serviceWorker from "./serviceWorker";
+// library.add(fas);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+// // If you wan1t your app to work offline and load faster, you can change
+// // unregister() to register()1111 b111e111111low. Note this comes with some pitfalls.
+// // Learn more about servi111111c2111e111 workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
